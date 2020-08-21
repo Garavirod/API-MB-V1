1ad33b0ff175476aa94a96864cc178f25ca0d83d
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-08-21 16:02:28 -0500

    API

---
+++ public/precache-manifest.fd8f8d898301cc489dda574fa35f625a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f915dd784705214b73e1873155a1b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96bd64cd1dae59bedd7f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b7e7babfcdcfafe170f9",
+    "url": "/static/js/2.8f633d88.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8f633d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96bd64cd1dae59bedd7f",
+    "url": "/static/js/main.09dab21f.chunk.js"
+  },
+  {
+    "revision": "a0f29b81eb07ca3028e2",
+    "url": "/static/js/runtime-main.1b847b2b.js"
+  }
+]);
